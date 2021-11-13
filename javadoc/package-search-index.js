da3a21adac313bfcd30cede4e0634f7227529df1
funkycitizen24 <78492729+funkycitizen24@users.noreply.github.com>
2021-11-14 00:51:52 +0200

    generated Javadocs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.company"},{"l":"com.company.controller"},{"l":"com.company.model"},{"l":"com.company.repository"},{"l":"com.company.view"}];updateSearchResults();
